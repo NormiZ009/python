22d7c906aefee2389f70f3b726d79f7e577a94b3
NormiZ009 <161232632+NormiZ009@users.noreply.github.com>
2024-05-08 11:12:47 +0530

    Add files via upload

---
+++ compare.py
@@ -0,0 +1,7 @@
+no_1, no_2 = input("enter the number: ").split(",")
+if int(no_1)>int(no_2):
+    print("no1 is greater than no2")
+elif int(no_1)<int(no_2):
+    print("no2 is greater than no1")
+else:
+    print("no1 is equal to no2")
---
+++ vote.py
@@ -0,0 +1,5 @@
+vote_age = int(input("enter your age: "))
+if vote_age>=18:
+    print("you can vote")
+else:
+    print("you can't vote")
